626af1826b1378baf143ca034c353e93a417309e
pranav mete <pranavmete57@gamil.com>
2025-02-13 18:12:46 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical bot',
+    version= '0.0.0',
+    author= 'Pranav Mete',
+    author_email= 'PranavMete57@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
